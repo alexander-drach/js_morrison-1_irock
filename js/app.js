503447fbb42139d040fc150f51ea826ac11b8c96
Alexander Drach <alexander.drach.adci@gmail.com>
2023-09-08 11:44:03 +0600

    add greeting irock

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log('irock');
+window.addEventListener('load', () => {
+  const irock = document.querySelector('.irock-wrapper img');
+
+   setTimeout(() => {
+    alert('Hello, I am your pet rock');
+   }, 2000);
+
+   irock.addEventListener('click', () => {
+    let userName = prompt('what is your name?', '');
+    alert(`it is good to meet you, ${userName}`);
+    irock.src = '../images/irock-smile.png';
+   })
+})
